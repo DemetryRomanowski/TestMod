0fea910420c24de274bc8926e38e4ff0f627a5f5
Zeoic <zeorgaming@gmail.com>
2015-05-16 01:07:02 -0400

    initial commit

---
+++ Mod/src/main/java/ca/zeor/modtest/items/TestModItem.java
@@ -0,0 +1,14 @@
+package ca.zeor.modtest.items;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import ca.zeor.modtest.help.Reference;
+
+public class TestModItem extends Item
+{
+	public TestModItem()
+	{
+		super();
+		setCreativeTab(CreativeTabs.tabMisc);
+	}
+}
